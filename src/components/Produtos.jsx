99eef6be60c5608283f2a2b987b5dd06c189baca
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-08-12 15:21:32 -0300

    feat: produtos

---
+++ src/components/Produtos.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Produtos.module.css";
+import Head from "./Head";
+
+const Produtos = () => {
+  const [dados, setDados] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    url();
+  }, []);
+
+  async function url() {
+    try {
+      setLoading(true);
+      const response = await (
+        await fetch(`https://ranekapi.origamid.dev/json/api/produto`)
+      ).json();
+      setDados(response);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <Head title="Produtos" descripition="descrição de Produtos" />
+      {loading && <p>Carregando...</p>}
+      <div className={`${styles.content} ${"animeLeft"}`}>
+        {dados &&
+          dados.map((produto) => (
+            <Link
+              style={{ textDecoration: "none" }}
+              to={`produto/${produto.id}`}
+              key={produto.id}
+            >
+              <img
+                className={styles.img}
+                src={produto?.fotos[0].src}
+                alt={produto?.fotos[0].alt}
+              />
+              <h2 className={styles.title}>{produto.nome}</h2>
+            </Link>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default Produtos;
