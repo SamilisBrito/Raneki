558ec5c0e17d0ceb75072c6454519a1a339d85f1
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-08-12 15:19:26 -0300

    feat: rodapé

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer style={{textAlign: "center", color: "#3e3e3e", fontWeight: "500", marginTop: "40px"}}>Alguns direitos reservado</footer>
+  )
+}
+
+export default Footer
