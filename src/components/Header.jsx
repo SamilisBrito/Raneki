7a66e7a55e0a1dd66372de92a3444da0319ed725
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-08-12 17:15:15 -0300

    wip: css modulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
 
 const Header = () => {
-  const activeStyle = {
-    backgroundColor: "#D7D7D7",
-  };
-
-  const baseStyle = {
-    padding: "10px",
-    backgroundColor: "#ECECEC",
-    textDecoration: "none",
-    color: "#202020",
-    borderRadius: "4px",
-    fontWeight: "500",
-  };
 
   return (
-    <header style={{marginBottom: '20px'}}>
-      <nav style={{ display: "flex", gap: "20px" }}>
-        <NavLink
-          to="/"
-          end
-          style={({ isActive }) =>
-            isActive ? { ...baseStyle, ...activeStyle } : baseStyle
-          }
-        >
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <NavLink className={styles.link} to="/" end>
           Produtos
         </NavLink>
-        <NavLink
-          to="contato"
-          style={({ isActive }) =>
-            isActive ? { ...baseStyle, ...activeStyle } : baseStyle
-          }
-        >
+        <NavLink className={styles.link} to="contato">
           Contato
         </NavLink>
       </nav>
